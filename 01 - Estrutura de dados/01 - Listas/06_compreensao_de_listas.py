5492d859188a8b23bdc899adfb89d369b87bb72f
Andre-MCarvalho <amc.tomoe@gmail.com>
2025-04-17 15:22:17 -0300

    adict: change comprensao_de_listas

--- 01 - Estrutura de dados/01 - Listas/06_compreensao_de_listas.py
+++ 01 - Estrutura de dados/01 - Listas/06_compreensao_de_listas.py
@@ -1,3 +1,10 @@
+numeros =[1,30,21,2,9,65,34]
+pares =[]
+for numero in numeros:
+    if numero %2==0:
+        pares.append(numero)
+print (pares)
+
 # Filtrar lista
 numeros = [1, 30, 21, 2, 9, 65, 34]
 pares = [numero for numero in numeros if numero % 2 == 0]
