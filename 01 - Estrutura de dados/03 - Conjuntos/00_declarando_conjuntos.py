d3264fbad351fb123e3f06d5e475b82f0a9a06ed
Andre-MCarvalho <amc.tomoe@gmail.com>
2025-04-24 09:30:36 -0300

    alterraçẽos para mehor entendimento

--- 01 - Estrutura de dados/03 - Conjuntos/00_declarando_conjuntos.py
+++ 01 - Estrutura de dados/03 - Conjuntos/00_declarando_conjuntos.py
@@ -7,3 +7,7 @@ print(letras)  # {"b", "a", "c", "x", "i"}
 
 carros = set(("palio", "gol", "celta", "palio"))
 print(carros)  # {"gol", "celta", "palio"}
+
+#pode ser declarado com chaves
+linguagens = {'python', 'java', 'python', 'c++','javascript','c++','c#'}
+print (linguagens)
