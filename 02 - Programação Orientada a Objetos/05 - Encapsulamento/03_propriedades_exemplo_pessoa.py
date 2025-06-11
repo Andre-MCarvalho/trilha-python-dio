867dd5bafb07eef2fc1ff87c27c81c5ae406319f
Andre Carvalho <amc.tomoe@gmail.com>
2025-06-11 10:24:51 -0300

    adiconado: outras possiblidades

--- 02 - Programação Orientada a Objetos/05 - Encapsulamento/03_propriedades_exemplo_pessoa.py
+++ 02 - Programação Orientada a Objetos/05 - Encapsulamento/03_propriedades_exemplo_pessoa.py
@@ -7,7 +7,15 @@ class Pessoa:
     def idade(self):
         _ano_atual = 2022
         return _ano_atual - self._ano_nascimento
+    
+    def get_nome(self):
+        return self.nome
+    
+    def get_idade(self):
+        return self.idade
+    
 
 
 pessoa = Pessoa("Guilherme", 1994)
 print(f"Nome: {pessoa.nome} \tIdade: {pessoa.idade}")
+print(f"Nome: {pessoa.get_nome()} \tIdade: {pessoa.get_idade()}")
