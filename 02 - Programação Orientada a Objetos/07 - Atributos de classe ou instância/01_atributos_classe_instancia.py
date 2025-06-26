223b2d2a8c7557d67a560a89b16495060e5f51bd
Andre Carvalho <amc.tomoe@gmail.com>
2025-06-26 16:05:53 -0300

    modificação: adicionado nota

--- 02 - Programação Orientada a Objetos/07 - Atributos de classe ou instância/01_atributos_classe_instancia.py
+++ 02 - Programação Orientada a Objetos/07 - Atributos de classe ou instância/01_atributos_classe_instancia.py
@@ -19,6 +19,8 @@ aluno_2 = Estudante("Giovanna", 2)
 mostrar_valores(aluno_1, aluno_2)
 #NOTA: Atributos de classe são compartilhados entre todas as instâncias da classe.
 # Se alterarmos o atributo de classe, todas as instâncias refletirão essa mudança.
+# Se alterarmos o atributo de instância, apenas aquela instância refletirá a mudança.
+
 Estudante.escola = "Python"
 aluno_3 = Estudante("Chappie", 3)
 mostrar_valores(aluno_1, aluno_2, aluno_3)
